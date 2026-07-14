91bf648656ee5f7f289a5f4c435a3cce74efac1f
timuravlyakulov <avlyakulovtimur@gmail.com>
2026-07-14 16:48:19 +0200

    add example with reentrant lock

--- src/main/java/avlyakulov/timur/practise/Practise8_1.java
+++ src/main/java/avlyakulov/timur/practise/Practise8_1.java
@@ -4,13 +4,15 @@ import lombok.SneakyThrows;
 
 import java.util.concurrent.TimeUnit;
 
-public class Practise8 {
+public class Practise8_1 {
 
     private static final int amount = 10;
+    private static final int secondsToSleep = 200;
 
-    //Задача 15. Банкомат и ReentrantLock
+    //Задача 15. Банкомат и ReentrantLock. Вариант с synchronized
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount();
+
         Thread client1 = new Thread(createRunnable(bankAccount, amount));
         Thread client2 = new Thread(createRunnable(bankAccount, amount));
         Thread client3 = new Thread(createRunnable(bankAccount, amount));
@@ -37,12 +39,13 @@ public class Practise8 {
         public synchronized boolean withdraw(int amountToWithdraw) {
             System.out.println(Thread.currentThread().getName() + " пытается снять деньги " + amountToWithdraw);
             if (balance >= amountToWithdraw) {
-                TimeUnit.MILLISECONDS.sleep(200);
                 balance -= amountToWithdraw;
                 System.out.println(Thread.currentThread().getName() + " снял успешно деньги остаток " + balance);
+                TimeUnit.MILLISECONDS.sleep(secondsToSleep);
                 return true;
             } else {
                 System.out.println("Недостаточно средств");
+                TimeUnit.MILLISECONDS.sleep(secondsToSleep);
                 return false;
             }
         }
