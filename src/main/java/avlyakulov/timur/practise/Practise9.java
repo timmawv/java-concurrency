e097a912ae257906465eb4fc9984db0cdf4b7692
timuravlyakulov <avlyakulovtimur@gmail.com>
2026-07-14 17:52:15 +0200

    add workpiece for 16-th task

---
+++ src/main/java/avlyakulov/timur/practise/Practise9.java
@@ -0,0 +1,60 @@
+package avlyakulov.timur.practise;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Practise9 {
+
+    private static final int amount = 10;
+    private static final int secondsToSleep = 200;
+
+    //Задача 16. tryLock()
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+
+        Thread client1 = new Thread(createRunnable(bankAccount));
+        Thread client2 = new Thread(createRunnable(bankAccount));
+        Thread client3 = new Thread(createRunnable(bankAccount));
+        Thread client4 = new Thread(createRunnable(bankAccount));
+
+        client1.start();
+        client2.start();
+        client3.start();
+        client4.start();
+    }
+
+    private static Runnable createRunnable(BankAccount bankAccount) {
+        return () -> {
+            while (!Thread.currentThread().isInterrupted())
+                bankAccount.withdraw(amount);
+        };
+    }
+
+    static class BankAccount {
+
+        private int balance = 100;
+        private final ReentrantLock lock = new ReentrantLock();
+
+        @SneakyThrows
+        public boolean withdraw(int amountToWithdraw) {
+            System.out.println(Thread.currentThread().getName() + " пытается снять деньги " + amountToWithdraw);
+            if (balance >= amountToWithdraw) {
+                lock.tryLock(2, TimeUnit.SECONDS);
+                try {
+                    balance -= amountToWithdraw; // проблема этого действия, что это не одно действия а 3, поэтому без блокировок не обойтись
+                } finally {
+                    lock.unlock();
+                }
+                System.out.println(Thread.currentThread().getName() + " снял успешно деньги остаток " + balance);
+                TimeUnit.MILLISECONDS.sleep(secondsToSleep);
+                return true;
+            } else {
+                System.out.println("Недостаточно средств");
+                TimeUnit.MILLISECONDS.sleep(secondsToSleep);
+                return false;
+            }
+        }
+    }
+}
