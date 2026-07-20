74dc2a3373e38451d52cd71a746bca6af9f9e02b
timuravlyakulov <avlyakulovtimur@gmail.com>
2026-07-20 18:56:07 +0200

    add almost done transfer service

--- src/main/java/avlyakulov/timur/practise/Practise12.java
+++ src/main/java/avlyakulov/timur/practise/Practise12.java
@@ -2,8 +2,11 @@ package avlyakulov.timur.practise;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.SneakyThrows;
 
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.IntStream;
 
@@ -26,7 +29,50 @@ public class Practise12 {
     }
 
     private static void makeTransfer(List<Account> accounts) {
+        Random random = new Random();
+        int firstAccount = random.nextInt(numberAccounts + 1);
+        int secondAccount = random.nextInt(numberAccounts + 1);
+        int amount = random.nextInt(1, 51); //от 1 до 51
+        if (firstAccount != secondAccount) {
+            lockAccountsAndMakeTransfer(firstAccount, secondAccount, accounts, amount);
+        }
+    }
 
+    //todo refactor it, we can in a previous step get number of account
+    @SneakyThrows
+    private static void lockAccountsAndMakeTransfer(int firstAccount, int secondAccount, List<Account> accounts, int amount) {
+        Account from = accounts.get(firstAccount);
+        Account to = accounts.get(secondAccount);
+        ReentrantLock fromLock = from.getReentrantLock();
+        ReentrantLock toLock = to.getReentrantLock();
+        fromLock.lock();
+        toLock.lock();
+        try {
+            if (canAccountSendMoney(amount, from.getBalance())) {
+                removeMoneyFromAccount(from, amount);
+                addMoneyToAccount(to, amount);
+                TimeUnit.SECONDS.sleep(2);
+            }
+        } finally {
+            fromLock.unlock();
+            toLock.unlock();
+        }
+    }
+
+    private static boolean canAccountSendMoney(int amount, int balanceAccount) {
+        return balanceAccount - amount > 0;
+    }
+
+    private static void removeMoneyFromAccount(Account from, int amount) {
+        int balance = from.getBalance();
+        int newBalance = balance - amount;
+        from.setBalance(newBalance);
+    }
+
+    private static void addMoneyToAccount(Account to, int amount) {
+        int balance = to.getBalance();
+        int newBalance = balance + amount;
+        to.setBalance(newBalance);
     }
 
     private static List<Account> createAccounts(int numberOfAccount) {
