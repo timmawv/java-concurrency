bb8168b54d40170fa5d28cdb37a6d65daadd7a7c
timuravlyakulov <avlyakulovtimur@gmail.com>
2026-07-21 17:34:50 +0200

    fixed todo and wrote the main logic

--- src/main/java/avlyakulov/timur/practise/Practise12.java
+++ src/main/java/avlyakulov/timur/practise/Practise12.java
@@ -1,12 +1,15 @@
 package avlyakulov.timur.practise;
 
+import avlyakulov.timur.utils.LoggerColor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.IntStream;
 
@@ -18,6 +21,8 @@ public class Practise12 {
     //Задача 19. Переводы между банковскими счетами
     public static void main(String[] args) {
         List<Account> accounts = createAccounts(numberAccounts);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        accounts.forEach(a -> executorService.execute(createBankWorker(accounts)));
     }
 
     private static Runnable createBankWorker(List<Account> accounts) {
@@ -30,28 +35,30 @@ public class Practise12 {
 
     private static void makeTransfer(List<Account> accounts) {
         Random random = new Random();
-        int firstAccount = random.nextInt(numberAccounts + 1);
-        int secondAccount = random.nextInt(numberAccounts + 1);
+        int fromId = random.nextInt(numberAccounts);
+        int toId = random.nextInt(numberAccounts);
+        Account from = accounts.get(fromId);
+        Account to = accounts.get(toId);
         int amount = random.nextInt(1, 51); //от 1 до 51
-        if (firstAccount != secondAccount) {
-            lockAccountsAndMakeTransfer(firstAccount, secondAccount, accounts, amount);
+        if (fromId != toId) {
+            lockAccountsAndMakeTransfer(from, to, amount);
         }
     }
 
-    //todo refactor it, we can in a previous step get number of account
     @SneakyThrows
-    private static void lockAccountsAndMakeTransfer(int firstAccount, int secondAccount, List<Account> accounts, int amount) {
-        Account from = accounts.get(firstAccount);
-        Account to = accounts.get(secondAccount);
+    private static void lockAccountsAndMakeTransfer(Account from, Account to, int amount) {
         ReentrantLock fromLock = from.getReentrantLock();
         ReentrantLock toLock = to.getReentrantLock();
         fromLock.lock();
         toLock.lock();
         try {
+            System.out.printf("Работник Банка - %s. Перевод между (%s, %s). Баланс отправителя (id = %s) %s, Баланс получателя (id = %s) %s, Сумма перевода %s. Время перевода %s\n", Thread.currentThread().getName(), from.getId(), to.getId(), from.getId(), from.getBalance(), to.getId(), to.getBalance(), amount, LocalDateTime.now().toLocalTime());
             if (canAccountSendMoney(amount, from.getBalance())) {
                 removeMoneyFromAccount(from, amount);
                 addMoneyToAccount(to, amount);
-                TimeUnit.SECONDS.sleep(2);
+                System.out.printf("Работник Банка - %s. Перевод между (%s, %s). После перевода. Баланс отправителя (id = %s) %s, Баланс получателя (id = %s) %s\n", Thread.currentThread().getName(), from.getId(), to.getId(), from.getId(), from.getBalance(), to.getId(), to.getBalance());
+            } else {
+                LoggerColor.printMessageWithColor("У отправителя недостаточно денег " + from.getId(), LoggerColor.Color.RED);
             }
         } finally {
             fromLock.unlock();
