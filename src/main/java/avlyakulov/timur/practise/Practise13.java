681ad783f8db4a3e6fe41ceaf0191648b50dbc59
timuravlyakulov <avlyakulovtimur@gmail.com>
2026-07-24 17:49:35 +0200

    add some code. Task is done by 65%

--- src/main/java/avlyakulov/timur/practise/Practise13.java
+++ src/main/java/avlyakulov/timur/practise/Practise13.java
@@ -8,20 +8,61 @@ import lombok.ToString;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
 
 public class Practise13 {
 
     private static final Lock queueLock = new ReentrantLock();
+    private static final Condition condition = queueLock.newCondition();
+
     private static final Random random = new Random();
     private static final int numberOfClient = 6;
+    private static final int balanceClient = 300;
 
     // Финальная задача. Мини-Банк
     @SneakyThrows
     public static void main(String[] args) {
+        Bank bank = new Bank();
+        List<Account> accounts = createAccounts(numberOfClient);
         Queue<OperationType> queue = new ArrayDeque<>();
         createThreadDemon(queue);
+    }
+
+    public static List<Account> createAccounts(int numberOfAccount) {
+        return IntStream.rangeClosed(1, numberOfAccount).mapToObj(id -> new Account(id, balanceClient, new ReentrantLock())).toList();
+    }
+
+    public static Runnable createBankWorker(Queue<OperationType> queue, Bank bank) {
+        return () -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                makeBankWork(queue, bank);
+            }
+        };
+    }
+
+    @SneakyThrows
+    public static void makeBankWork(Queue<OperationType> queue, Bank bank) {
+        OperationType operationFromQueue = getOperationFromQueue(queue);
+    }
+
+    @SneakyThrows
+    private static OperationType getOperationFromQueue(Queue<OperationType> queue) {
+        queueLock.lock();
+        try {
+            while (queue.isEmpty()) {
+                condition.await();
+            }
+            return queue.poll();
+        } finally {
+            queueLock.unlock();
+        }
+    }
+
+    public static void doBankWork() {
+
     }
 
     public static void createThreadDemon(Queue<OperationType> queue) {
@@ -37,33 +78,33 @@ public class Practise13 {
     }
 
     public static void putTask(Queue<OperationType> queue) {
+        OperationType operationType;
         queueLock.lock();
         try {
-            putTypeTask(queue);
+            operationType = putTypeTask(queue);
+            condition.signal();
         } finally {
             queueLock.unlock();
         }
+        takeBreak(operationType);
     }
 
-    public static void putTypeTask(Queue<OperationType> queue) {
-        int taskNumber = random.nextInt(1, 3);
-        switch (taskNumber) {
-            case 1 -> putTransferInQueue(queue);
-            case 2 -> putPrintStatisticInQueue(queue);
-            default -> throw new UnsupportedOperationException();
+    @SneakyThrows
+    public static void takeBreak(OperationType operationType) {
+        switch (operationType) {
+            case TRANSFER -> TimeUnit.MILLISECONDS.sleep(400);
+            case STATISTIC -> TimeUnit.SECONDS.sleep(3);
         }
     }
 
-    @SneakyThrows
-    public static void putTransferInQueue(Queue<OperationType> queue) {
-        queue.add(OperationType.TRANSFER);
-        TimeUnit.MILLISECONDS.sleep(400);
-    }
-
-    @SneakyThrows
-    public static void putPrintStatisticInQueue(Queue<OperationType> queue) {
-        queue.add(OperationType.STATISTIC);
-        TimeUnit.SECONDS.sleep(5);
+    public static OperationType putTypeTask(Queue<OperationType> queue) {
+        int taskNumber = random.nextInt(1, 3);
+        switch (taskNumber) {
+            case 1 -> queue.add(OperationType.TRANSFER);
+            case 2 -> queue.add(OperationType.STATISTIC);
+            default -> throw new UnsupportedOperationException();
+        }
+        return taskNumber == 1 ? OperationType.TRANSFER : OperationType.STATISTIC;
     }
 
 
@@ -81,7 +122,11 @@ public class Practise13 {
         }
 
         public void withdraw(int amount) {
+            balance += amount;
+        }
 
+        public boolean canDeposit(int amount) {
+            return balance - amount >= 0;
         }
     }
 
@@ -104,9 +149,10 @@ public class Practise13 {
     static class Bank {
 
         private List<BankOperation> bankOperations = new ArrayList<>();
+        private final Lock lock = new ReentrantLock();
 
-        public void transfer(Account from, Account to) {
-
+        public void putTransfer(BankOperation bankOperation) {
+            bankOperations.add(bankOperation);
         }
 
         public void printTransfers() {
