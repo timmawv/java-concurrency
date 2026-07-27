5123b56c4b5928c7d271adf20a86f5ab83a99306
timuravlyakulov <avlyakulovtimur@gmail.com>
2026-07-27 17:11:35 +0200

    Task done. 95% just need better to check work and make small refactoring

--- src/main/java/avlyakulov/timur/practise/Practise13.java
+++ src/main/java/avlyakulov/timur/practise/Practise13.java
@@ -1,12 +1,11 @@
 package avlyakulov.timur.practise;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.SneakyThrows;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -21,6 +20,7 @@ public class Practise13 {
     private static final Random random = new Random();
     private static final int numberOfClient = 6;
     private static final int balanceClient = 300;
+    private static final int amountToTransfer = 30;
 
     // Финальная задача. Мини-Банк
     @SneakyThrows
@@ -29,23 +29,26 @@ public class Practise13 {
         List<Account> accounts = createAccounts(numberOfClient);
         Queue<OperationType> queue = new ArrayDeque<>();
         createThreadDemon(queue);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        IntStream.rangeClosed(1, 3).forEach(i -> executorService.execute(createBankWorker(queue, bank, accounts)));
     }
 
     public static List<Account> createAccounts(int numberOfAccount) {
         return IntStream.rangeClosed(1, numberOfAccount).mapToObj(id -> new Account(id, balanceClient, new ReentrantLock())).toList();
     }
 
-    public static Runnable createBankWorker(Queue<OperationType> queue, Bank bank) {
+    public static Runnable createBankWorker(Queue<OperationType> queue, Bank bank, List<Account> accounts) {
         return () -> {
             while (!Thread.currentThread().isInterrupted()) {
-                makeBankWork(queue, bank);
+                makeBankWork(queue, bank, accounts);
             }
         };
     }
 
     @SneakyThrows
-    public static void makeBankWork(Queue<OperationType> queue, Bank bank) {
+    public static void makeBankWork(Queue<OperationType> queue, Bank bank, List<Account> accounts) {
         OperationType operationFromQueue = getOperationFromQueue(queue);
+        doBankWork(operationFromQueue, bank, accounts);
     }
 
     @SneakyThrows
@@ -61,8 +64,71 @@ public class Practise13 {
         }
     }
 
-    public static void doBankWork() {
+    public static void doBankWork(OperationType operationFromQueue, Bank bank, List<Account> accounts) {
+        switch (operationFromQueue) {
+            case TRANSFER -> makeTransfer(bank, accounts);
+            case STATISTIC -> printOperations(bank);
+        }
+    }
 
+    private static void printOperations(Bank bank) {
+        Lock lock = bank.getLock();
+        lock.lock();
+        try {
+            bank.printTransfers();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private static void makeTransfer(Bank bank, List<Account> accounts) {
+        int fromId = random.nextInt(1, numberOfClient);
+        int toId = random.nextInt(1, numberOfClient);
+        if (fromId != toId) {
+            Account from = accounts.get(fromId);
+            Account to = accounts.get(toId);
+            lockForTransfer(from, to);
+            try {
+                completeTransfer(from, to);
+                putToBankOperation(bank, from, to);
+            } finally {
+                from.getLock().unlock();
+                to.getLock().unlock();
+            }
+        }
+    }
+
+    private static void lockForTransfer(Account from, Account to) {
+        int fromId = from.getId();
+        int toId = to.getId();
+        if (fromId < toId) {
+            from.getLock().lock();
+            to.getLock().lock();
+        } else {
+            to.getLock().lock();
+            from.getLock().lock();
+        }
+    }
+
+    private static void completeTransfer(Account from, Account to) {
+        from.withdraw(amountToTransfer);
+        to.deposit(amountToTransfer);
+    }
+
+    private static void putToBankOperation(Bank bank, Account from, Account to) {
+        bank.getLock().lock();
+        try {
+            BankOperation bankOperation = BankOperation.builder()
+                    .operationId(bank.getAndIncrementCounter())
+                    .fromAccountId(from.getId())
+                    .toAccountId(to.getId())
+                    .amount(amountToTransfer)
+                    .createdAt(LocalDateTime.now())
+                    .build();
+            bank.putTransfer(bankOperation);
+        } finally {
+            bank.getLock().unlock();
+        }
     }
 
     public static void createThreadDemon(Queue<OperationType> queue) {
@@ -118,19 +184,21 @@ public class Practise13 {
         private Lock lock;
 
         public void deposit(int amount) {
-
-        }
-
-        public void withdraw(int amount) {
             balance += amount;
         }
 
+        public void withdraw(int amount) {
+            balance -= amount;
+        }
+
+        //todo add this method also
         public boolean canDeposit(int amount) {
             return balance - amount >= 0;
         }
     }
 
     @Getter
+    @Builder
     @AllArgsConstructor
     @ToString
     static class BankOperation {
@@ -150,6 +218,7 @@ public class Practise13 {
 
         private List<BankOperation> bankOperations = new ArrayList<>();
         private final Lock lock = new ReentrantLock();
+        private int counterOperationId = 0;
 
         public void putTransfer(BankOperation bankOperation) {
             bankOperations.add(bankOperation);
@@ -157,6 +226,10 @@ public class Practise13 {
 
         public void printTransfers() {
             System.out.println(bankOperations);
+        }
+
+        public int getAndIncrementCounter() {
+            return ++counterOperationId;
         }
     }
 }
