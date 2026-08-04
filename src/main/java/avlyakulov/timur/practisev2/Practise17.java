4c23ee4cb6df9c04207105dd0f42270d77b6a6d8
timuravlyakulov <avlyakulovtimur@gmail.com>
2026-08-04 16:45:03 +0200

    finished task with CyclicBarrier

--- src/main/java/avlyakulov/timur/practisev2/Practise17.java
+++ src/main/java/avlyakulov/timur/practisev2/Practise17.java
@@ -1,10 +1,39 @@
 package avlyakulov.timur.practisev2;
 
+import avlyakulov.timur.utils.LoggerColor;
+
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
+
 public class Practise17 {
 
     //Задача 27. Автогонки ⭐⭐
     //CyclicBarrier
     public static void main(String[] args) {
+        int cars = 6;
+        //тут был вызван Runnable 2 аргументом, он вызывается, до розблокировки всех потоков и отрабатывает, потом потоки разблокируются и работают дальше
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(6, () -> LoggerColor.printMessageWithColor("The race has started", LoggerColor.Color.GREEN));
+        ExecutorService executorService = Executors.newFixedThreadPool(cars);
+        IntStream.rangeClosed(1, cars).forEach(i -> executorService.execute(createCar(cyclicBarrier)));
+        executorService.shutdown();
+    }
 
+    private static Runnable createCar(CyclicBarrier cyclicBarrier) {
+        return () -> {
+            try {
+                TimeUnit.SECONDS.sleep(getRandomNumber());
+                LoggerColor.printMessageWithColor(Thread.currentThread().getName().concat(" has arrived to start line"), LoggerColor.Color.YELLOW);
+                cyclicBarrier.await();
+                LoggerColor.printMessageWithColor(Thread.currentThread().getName().concat(" has started the race"), LoggerColor.Color.BLUE);
+                TimeUnit.SECONDS.sleep(getRandomNumber());
+                LoggerColor.printMessageWithColor(Thread.currentThread().getName().concat(" has arrived to finish line"), LoggerColor.Color.GREEN);
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+            }
+        };
+    }
+
+    private static int getRandomNumber() {
+        return ThreadLocalRandom.current().nextInt(1, 5);
     }
 }
--- src/main/java/avlyakulov/timur/utils/LoggerColor.java
+++ src/main/java/avlyakulov/timur/utils/LoggerColor.java
@@ -6,16 +6,18 @@ public class LoggerColor {
     private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_GREEN = "\u001B[32m";
     private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
 
     public static void printMessageWithColor(String message, Color color) {
         switch (color) {
             case RED -> System.out.println(ANSI_RED + message + ANSI_RESET);
             case GREEN -> System.out.println(ANSI_GREEN + message + ANSI_RESET);
             case YELLOW -> System.out.println(ANSI_YELLOW + message + ANSI_RESET);
+            case BLUE -> System.out.println(ANSI_BLUE + message + ANSI_RESET);
         }
     }
 
     public enum Color {
-        RED, GREEN, YELLOW
+        RED, GREEN, YELLOW, BLUE
     }
 }
